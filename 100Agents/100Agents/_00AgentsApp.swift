0419f3eb0ae1fcea2477eef48104674d2f959bc6
Brayton-Lordianto <braylord30@gmail.com>
2025-06-26 15:37:55 -0700

    Initial commit

---
+++ 100Agents/100Agents/_00AgentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00AgentsApp.swift
+//  100Agents
+//
+//  Created by Brayton Lordianto on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct _00AgentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
